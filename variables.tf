03f07cfc3df6c90662e439434791e7ad5e436a11
Ishitham <81800391+Ishitham@users.noreply.github.com>
2025-12-03 13:40:05 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "IshithaM"
+  default     = "ishitham"
 }
 
 variable "class_name" {
