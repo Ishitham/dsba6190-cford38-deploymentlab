3f4867120fc29bebecb7ba82f7be716432f102bc
Ishitham <81800391+Ishitham@users.noreply.github.com>
2025-12-03 14:40:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,5 +43,50 @@ resource "azurerm_storage_account" "storage" {
   tags = local.tags
 }
 
+  # SQL Server
+resource "azurerm_mssql_server" "sql" {
+  name                         = "sql${random_integer.deployment_id_suffix.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+
+  administrator_login          = "sqladminuser"
+  administrator_login_password = "ChangeThisP@ssword123!"  
+}
+
+# SQL Database
+resource "azurerm_mssql_database" "sqldb" {
+  name        = "sqldb${random_integer.deployment_id_suffix.result}"
+  server_id   = azurerm_mssql_server.sql.id
+  sku_name    = "Basic"      # or "S0" if your lab wants Standard
+  max_size_gb = 2
+}
+
+# Allow SQL only from your vNet subnet
+resource "azurerm_mssql_virtual_network_rule" "sql_vnet_rule" {
+  name      = "sql-vnet-rule-${random_integer.deployment_id_suffix.result}"
+  server_id = azurerm_mssql_server.sql.id
+  subnet_id = azurerm_subnet.subnet.id
+}
+
+# Network Security Group for the VM (allow SSH)
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "nsg-vm-${random_integer.deployment_id_suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 
 ##
